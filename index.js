cf6e0f5e7b6f9f1c3ba9e575e51ba6398456383d
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-02-16 22:55:52 +0800

    update to make grid to receive only 1 value

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const sketchPad = document.querySelector(".sketchPad");
 const container = document.getElementById("container");
 
-//make a grid based on 2 values
-function makeRows(rows, cols) {
-    sketchPad.style.setProperty('--grid-rows', rows);
-    sketchPad.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    sketchPad.appendChild(cell).className = "grid-item";
+//make a grid based on 1 values
+function makeRows(e) {
+  for (c = 0; c < (e); c++) {
+    let col = document.createElement('div');
+    col.setAttribute("style","display: flex; flex-direction: row; flex-grow:1;");
+    sketchPad.appendChild(col);
+    for (i = 0; i < (e) ; i++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("style","aspect-ratio:1/1;");
+      col.appendChild(cell).className = "grid-item";
+    }
   };
 };
 
-makeRows(10, 10);
+makeRows(100);
+
+// //make a grid based on 2 values
+// function makeRows(rows, cols) {
+//     sketchPad.style.setProperty('--grid-rows', rows);
+//     sketchPad.style.setProperty('--grid-cols', cols);
+//   for (c = 0; c < (rows * cols); c++) {
+//     let cell = document.createElement("div");
+//     sketchPad.appendChild(cell).className = "grid-item";
+//   };
+// };
+
+// makeRows(10, 10);
