4929b1a71015ddf3bf52b28d9b0304be40145cd0
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-03-20 22:09:05 +0800

    Add range button Add functional range button

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const sketchPad = document.querySelector(".sketchPad");
 
+makeRows(30);
+
 //make a grid based on 1 values
 function makeRows(e) {
   for (c = 0; c < (e); c++) {
     let col = document.createElement('div');
     col.setAttribute("style","display: flex; flex-direction: row; flex-grow:1;");
-    sketchPad.appendChild(col);
+    sketchPad.appendChild(col).className = "grid-row";
     for (i = 0; i < (e) ; i++) {
       let cell = document.createElement("div");
       cell.setAttribute("style","aspect-ratio:1/1;");
@@ -18,16 +20,18 @@ sketchPad.addEventListener("mouseover", function(event) {
   event.target.style.backgroundColor = "black";
 });
 
-makeRows(50);
 
-// //make a grid based on 2 values
-// function makeRows(rows, cols) {
-//     sketchPad.style.setProperty('--grid-rows', rows);
-//     sketchPad.style.setProperty('--grid-cols', cols);
-//   for (c = 0; c < (rows * cols); c++) {
-//     let cell = document.createElement("div");
-//     sketchPad.appendChild(cell).className = "grid-item";
-//   };
-// };
+function updateProjectScore() {
+  let output = document.querySelector("output");
+  removeSketchPad();
+  makeRows(output.textContent);
+}
 
-// makeRows(10, 10);
+
+function removeSketchPad() {
+  lastChild = sketchPad.lastChild;
+  while (sketchPad.lastChild){
+    sketchPad.removeChild(sketchPad.lastChild);
+    console.log("entering");
+  }
+};
